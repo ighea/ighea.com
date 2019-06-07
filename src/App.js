1a986c7becd5fdddd141ad946553f08fc830a974
Mika Hynnä <mika.hynna@slm.fi>
2019-06-07 13:04:36 +0300

    Base structure working

--- src/App.js
+++ src/App.js
@@ -1,74 +1,68 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, withRouter } from "react-router-dom";
 import './App.css';
 
 import MainPage from './pages/MainPage.js';
 import ExperiencePage from './pages/ExperiencePage.js';
 import ProjectsPage from './pages/ProjectsPage.js';
+import NotFoundPage from './pages/NotFoundPage.js';
 
+class BaseApp extends React.Component {
 
-class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      show_big_banner: true,
-    }
-  }
-
-  handleNavigationEvent(value) {
-    let state = this.state;
-    state.show_big_banner = value;
-    this.setState(state);
   }
 
   render() {
+
+    const classes = (this.props.location.pathname === "/" ? 'App-header App-header-frontpage' : 'App-header');
+
     return (
       <div className="App">
 
-        <Router>
+        <header className={classes}>
 
-          <header className={!this.state.show_big_banner?'App-header':'App-header App-header-large'}>
+          <section className="App-header-main">
 
-            <section className="App-header-main">
+            <div>
+              <h1>Mika Hynnä</h1>
+              <p>Programmer, Web Developer and Tech Enthusiast!</p>
+            </div>
 
-              <div>
-                <h1>Mika Hynnä</h1>
-                <p>Programmer, Web Developer and Tech Enthusiast!</p>
-              </div>
+            <nav>
+              <ul>
+                <li><NavLink to="/" exact>Me!</NavLink></li>
+                <li><NavLink to="/experience">Experience</NavLink></li>
+                <li><NavLink to="/projects/">Projects</NavLink></li>
+              </ul>
+            </nav>
 
-              <nav>
-                <ul>
-                  <li><NavLink to="/" exact>Me!</NavLink></li>
-                  <li><NavLink to="/experience">Experience</NavLink></li>
-                  <li><NavLink to="/projects/">Projects</NavLink></li>
-                </ul>
-              </nav>
+          </section>
 
-            </section>
+        </header>
 
-          </header>
-
-          <main>
-            <Switch componentDidMount={() => console.log("SWITCH", this)}>
-              <Route exact path="/" component={MainPage} />
-              <Route exact path="/experience" component={ExperiencePage} />
-              <Route path="/projects/" component={ProjectsPage} />
-            </Switch>
-          </main>
+        <main>
+          <Switch componentDidMount={() => console.log("SWITCH", this)}>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/experience" component={ExperiencePage} />
+            <Route path="/projects/" component={ProjectsPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
 
 
-          <footer className="App-footer">
-            <ul>
-              <li><a href="https://www.linkedin.com/in/mika-hynna-07984a108/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
-              <li><a href="https://github.com/ighea" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-            </ul>
-          </footer>
-
-        </Router>
+        <footer className="App-footer">
+          <ul>
+            <li><a href="https://www.linkedin.com/in/mika-hynna-07984a108/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+            <li><a href="https://github.com/ighea" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+          </ul>
+        </footer>
 
       </div>
     );
   }
 }
+
+const App = withRouter(BaseApp);
 
 export default App;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const baseDivStyles = {
+  display: "block",
+}
+
+const boxStyles = {
+  display: "inline-block",
+  verticalAlign: "top",
+  marginRight: "30pt",
+}
+
+class NotFoundPage extends React.Component {
+
+
+  render() {
+    return (
+      <div className="PageContent">
+
+        <h2>404 - Page Not Found</h2>
+
+        <div style={baseDivStyles}>
+          <div style={boxStyles}>
+
+            <p>
+              It looks like the page you have requested was not found. <b>*sad face*</b>
+            </p>
+
+            <p>All hope is not lost!</p>
+
+            <p>
+              As here is something random from Youtube for you to enjoy!
+            </p>
+
+          </div>
+
+          <iframe width="500" height="315" src="https://www.youtube-nocookie.com/embed/mPVC2UJRAbc?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style={boxStyles}></iframe>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
