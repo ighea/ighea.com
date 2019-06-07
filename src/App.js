c672f025a41e9edf87426a048bf7516306a0a310
Mika Hynnä <mika.hynna@slm.fi>
2019-06-07 15:48:54 +0300

    Protected email

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import NotFoundPage from './pages/NotFoundPage.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faCheckSquare, faCoffee);
+import { faUserCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faUserCircle, faEnvelope);
 
 class BaseApp extends React.Component {
 
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,21 +1,51 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Mailto from 'react-protected-mailto'
+
+import image from '../assets/images/desk.jpg';
+
+const styles = {};
+
+styles.image = {
+  float: "right",
+  marginLeft: "40pt",
+  marginBottom: "30pt",
+  height: "auto",
+  maxWidth: "50%",
+}
+
 class ContactPage extends React.Component {
 
   render() {
     return (
       <div className="MainPage PageContent">
 
+
         <h2>Contact</h2>
 
+        <img src={image} alt="Oled showing Ighea powered by Raspberry PI3." style={styles.image} />
+
         <section>
-          <h3>Poke on IRC</h3>
+          <h3><FontAwesomeIcon icon="user-circle" /> Poke on IRC</h3>
           <p>You can find me on IRC on Freenode, IRCNet and QuakeNet networks by nick <b>ighea</b>.</p>
         </section>
 
         <section>
-          <h3>Hail through LinkedIn</h3>
+          <h3><FontAwesomeIcon icon={['fab', 'linkedin' ]} /> Hail through LinkedIn</h3>
           <p>Don&acute;t be afraid! Send me a message on <a href="https://www.linkedin.com/in/mika-hynna-07984a108/" target="_blank" rel="noopener noreferrer">LinkedIn</a>!</p>
+        </section>
+
+        <section>
+          <h3><FontAwesomeIcon icon="envelope" /> Send e-mail</h3>
+          <p>
+            to address <Mailto
+              email='mika.hynna@ighea.com'
+              headers={
+                {subject:'Question from the website ighea.com'}
+              }
+            />
+          </p>
         </section>
 
       </div>
