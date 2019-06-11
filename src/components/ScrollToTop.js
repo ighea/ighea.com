26168c876dbd0efb2b50056690a0cd902ba94457
Mika Hynnä <mika.hynna@slm.fi>
2019-06-11 12:00:10 +0300

    Remove space

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -5,8 +5,8 @@ import { withRouter } from "react-router-dom";
 class ScrollToTop extends React.Component {
 
   componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
+    if(this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
     }
   }
 
