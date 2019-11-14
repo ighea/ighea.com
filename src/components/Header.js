383b57c813a33530e103d2c563008ad19b5459ad
Mika Hynnä <mika.hynna@iki.fi>
2019-11-14 21:22:14 +0200

    Header.js: Fix scroll position on Firefox

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,8 @@ class Header extends React.Component {
   }
 
   handleScroll = (event) =>  {
-    const pos = event.pageY || ( /*chrome?*/ event.path && event.path[1] && event.path[1].scrollY) || 0;
+console.log(event);
+    const pos = event.pageY || window.scrollY || ( /*chrome?*/ event.path && event.path[1] && event.path[1].scrollY) || 0;
     this.setState({ scrollY: pos });
   };
 
